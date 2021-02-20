5ca6a464cb7f6895ebdf6541c87ad66f464a2588
Andrew <44880821+andpuc23@users.noreply.github.com>
2021-02-20 16:13:31 +0300

    Initial commit

---
+++ Tuttes_graphs/TuttesGraphsVisualise/Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿
+namespace TuttesGraphsVisualise
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose( bool disposing )
+        {
+            if ( disposing && ( components != null ) )
+            {
+                components.Dispose();
+            }
+            base.Dispose( disposing );
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.redrawBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // redrawBtn
+            // 
+            this.redrawBtn.Location = new System.Drawing.Point(1006, 1041);
+            this.redrawBtn.Name = "redrawBtn";
+            this.redrawBtn.Size = new System.Drawing.Size(156, 76);
+            this.redrawBtn.TabIndex = 0;
+            this.redrawBtn.Text = "Redraw";
+            this.redrawBtn.UseVisualStyleBackColor = true;
+            this.redrawBtn.Click += new System.EventHandler(this.redrawBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1174, 1129);
+            this.Controls.Add(this.redrawBtn);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button redrawBtn;
+    }
+}
+
---
+++ Tuttes_graphs/TuttesGraphsVisualise/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Tuttes_graphs;
+
+namespace TuttesGraphsVisualise
+{
+    public partial class Form1 : Form
+    {
+        Graph graph;
+        const int vertexHalfSize = 3;
+        int circleSize;
+        (int, int) centerCoords;
+        public Form1()
+        {
+            InitializeComponent();
+            Paint += DrawGraph;
+            circleSize = (int)(Math.Min( Size.Width, Size.Height ) / 3.0);
+            int center = circleSize * 3 / 2;
+            centerCoords = (center, center);
+
+            graph = Graph.FromJson( @"../../../graph.json" );
+            graph.GRAPH_SIZE = circleSize * 12 / 5;
+        }
+
+        public void DrawGraph(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            g.Clear(Color.White);
+            Pen p = new Pen( Color.Black );
+            Brush b = new SolidBrush( Color.Black );
+            for ( int i = 0; i < graph.VertexNum; ++i )
+            {
+                var v = graph.Vertices[ i ];
+                g.DrawEllipse( p, v.X - vertexHalfSize, v.Y - vertexHalfSize, 2 * vertexHalfSize, 2 * vertexHalfSize );
+                g.FillEllipse( b, v.X - vertexHalfSize, v.Y - vertexHalfSize, 2 * vertexHalfSize, 2 * vertexHalfSize );
+                g.DrawString( (i+1).ToString(), new Font( "calibri", 10 ), b, v.X - 10, v.Y );
+                foreach ( var n in v.Neighbors )
+                {
+                    if (graph.Vertices.IndexOf(n) > i )
+                        g.DrawLine( p, new Point( v.X, v.Y ), new Point( n.X, n.Y ) );
+                }
+            }
+        }
+
+        private void redrawBtn_Click( object sender, EventArgs e )
+        {
+            graph.GetCycle( out var cycle );
+            if ( cycle is null )
+            {
+                MessageBox.Show( "Graph is acyclic" );
+                return;
+            }
+            // расположить точки на окружности размером circle_sizе и центром center
+            int N = cycle.Count;
+            for (int i = 0; i < N; ++i )
+            {
+                double ro = circleSize;
+                double phi = (double) i / N * 2 * Math.PI;
+                double x = ro * Math.Cos( phi );
+                double y = ro * Math.Sin( phi );
+
+                cycle[ i ].X = (int)x + centerCoords.Item1;
+                cycle[ i ].Y = (int)y + centerCoords.Item2;
+            }
+
+            foreach (var v in graph.Vertices )
+            {
+                if ( cycle.Contains( v ) )
+                    continue;
+                v.X = (int)v.Neighbors.Select( n => n.X ).Average();
+                v.Y = (int) v.Neighbors.Select( n => n.Y ).Average();
+            }
+
+            this.Invalidate();
+        }
+    }
+}
